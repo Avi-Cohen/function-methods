7b889ade0cb14650ad76eae666209500f38600bc
Avi-Cohen <aviko.cohen@gmail.com>
2021-07-18 21:22:58 +0300

    global variable sample

--- samlpes.js
+++ samlpes.js
@@ -3,3 +3,17 @@
  * will cover the following topices:
  * this, call/bind/apply
  */
+
+// this:
+
+function a() {
+  console.log(this);
+  this.newVariable = 'hello' // possible ?? => yes, Good practice ?? => NO!!
+}
+a();
+
+const obj = {};
+obj.name = "Avi";
+console.log(obj);
+
+console.log(newVariable);
